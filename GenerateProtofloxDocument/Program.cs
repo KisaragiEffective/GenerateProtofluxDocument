2ef0ccf0838d38d57008b4dfbee2b0e37155fb7b
Kisaragi Marine <kisaragi.effective@gmail.com>
2023-10-10 21:32:50 +0900

    refactor: rewrite program to avoid implicit main

--- GenerateProtofloxDocument/Program.cs
+++ GenerateProtofloxDocument/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace GenerateProtofloxDocument;
 
-Console.WriteLine("Hello, World!");
+internal static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
